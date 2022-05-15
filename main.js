a769758e3eb43965a00cfbd2f3bcc542eff4153b
DevMattDavies <mdavies.me@pm.me>
2022-05-16 00:38:26 +0100

    Splash page complete, about me functionality complete (lorem ipsum text remains)

---
+++ main.js
@@ -0,0 +1,22 @@
+// Typed effect on splash page
+
+let typedSplash = new Typed(".typed-words-splash", {
+    strings: ["developer", "thinker", "problem_solver", "software_engineer", "learner", "designer", "project_manager", "developer"],
+    backDelay: 2500,
+    typeSpeed: 120,
+    backSpeed: 50,
+    loop: false,
+    shuffle: false
+
+})
+
+// Typed effect on about me page
+
+let typedAboutMe = new Typed(".typed-words-about-me", {
+    strings: [`<span style='color: var(--code-purple'>const </span><span style='color: var(--code-white'> name <span style='color: var(--code-equals-blue'>= <span style='color: var(--code-green'>'Matt'<span style='color: var(--code-white'>;<br><span style='color: var(--code-purple'>let <span style='color: var(--code-white'>age <span style='color: var(--code-equals-blue'>= <span style='color: var(--code-orange'> 33<span style='color: var(--code-white'>;<br><span style='color: var(--code-purple'>let <span style='color: var(--code-white'>experience <span style='color: var(--code-equals-blue'>= <span style='color: var(--code-yellow'>{<br><span style='color: var(--code-green'>coding<span style='color: var(--code-equals-blue'>: <span style='color: var(--code-green'>"school_of_code"<span style='color: var(--code-white'>,<br><span style='color: var(--code-green'>previous<span style='color: var(--code-equals-blue'>: <span style='color: var(--code-green'> "project_management"<br><span style='color: var(--code-yellow'>}<br><br><span style='color: var(--code-purple'>function <span style='color: var(--code-function-blue'>newDeveloper<span style='color: var(--code-yellow'>(<span style='color: var(--code-white'>name<span style='color: var(--code-yellow'>) {<br><span style='color: var(--code-purple'>let <span style='color: var(--code-white'>startCodingCareer <span style='color: var(--code-equals-blue'>=<br><span style='color: var(--code-purple'>(<span style='color: var(--code-white'>age <span style='color: var(--code-equals-blue'>* <span style='color: var(--code-white'>experience<span style='color: var(--code-equals-blue'>.<span style='color: var(--code-red'>previous<span style='color: var(--code-purple'>) <span style='color: var(--code-equals-blue'>* <span style='color: var(--code-white'>experience<span style='color: var(--code-equals-blue'>.<span style='color: var(--code-red'>coding<span style='color: var(--code-white'>;<br><span style='color: var(--code-purple'>return <span style='color: var(--code-white'>name <span style='color: var(--code-equals-blue'>+ <span style='color: var(--code-white'>startCodingCareer;<br><span style='color: var(--code-yellow'>}<br><br><span style='color: var(--code-function-blue'>newDeveloper<span style='color: var(--code-yellow'>(<span style='color: var(--code-green'>"Matt"<span style='color: var(--code-yellow'>)<span style='color: var(--code-white'>;`],
+    startDelay: 5000,
+    typeSpeed: 50,
+    loop: false,
+    shuffle: false
+
+})
